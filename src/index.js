3b98b5e385c1bf62d7b4ec9f77a39ffc9f4dfaa4
Rarity110 <rarity110@mail.ru>
2022-12-19 13:49:08 +0300

    feat: add decompress

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import { stdin as input, stdout as output, exit } from 'node:process';
 
 import { firstMessage, lastMessage } from './consts.js';
 import { currentDirectory } from './currentDirectory.js';
+import { ERROR_MESSAGE_INVALID_INPUT } from '../consts.js';
 
 import { cd, ls, cat, add, rn, cp, mv, rm } from './fs/index.js';
 import getOsInfo from './os.js';
 import getHash from './hash.js';
-import compress from './zip/compress.js';
-// import decompress from './decompress.js';
+import { compress, decompress} from './zip/index.js';
 
 const start = async () => {
-    console.log(process.argv);
     const rl = readline.createInterface({ input, output });
     rl.write(firstMessage);
     currentDirectory();
@@ -56,14 +55,14 @@ const start = async () => {
             case "compress":
                 compress(args);
                 break;
-            // case "decompress":
-            //     decompress(arg1, arg2);
-            //     break;
+            case "decompress":
+                decompress(args);
+                break;
             case '.exit':
                 exit();
                 // break;
             default:
-                // console.log(`Say what? I might have heard '${line.trim()}'`);
+                console.log(ERROR_MESSAGE_INVALID_INPUT);
                 break;
         }
         rl.prompt();
@@ -76,5 +75,4 @@ process.on('exit', (code) => {
     console.log(lastMessage);
 });
 
-console.log(process.argv);
 await start();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,4 +1,4 @@
-import { basename, resolve, resolveExtname } from 'path';
+import { basename, resolve } from 'path';
 import { pipeline } from 'stream/promises';
 import fs from 'fs';
 import zlib from 'zlib';
@@ -8,26 +8,33 @@ import argsPair from '../fs/helpers.js';
 import { ERROR_MESSAGE_OPERATION, ERROR_MESSAGE_INVALID_INPUT } from '../consts.js';
 
 const decompress = async (args) => {
-    const [ filePath, filePathCopy ] = argsPair(args);
-
-    const pathFileZip = resolve(cwd(), resolveExtname(filePath));
-    const fileName = basename(pathFileZip).replace('.br', '');
-    const pathNewFile = resolve(cwd(), filePathCopy, fileName);
-
     try {
+
+        const [ filePath, filePathCopy ] = argsPair(args);
+
+        const pathFileZip = resolve(cwd(), filePath);
+        const fileName = basename(pathFileZip).replace('.br', '');
+        const pathNewFile = resolve(cwd(), filePathCopy, fileName);
 
         fs.access(pathNewFile, fs.F_OK, (err) => {
             
             if(err) {
 
-                pipeline(
-                    fs.createReadStream(pathFileZip),
-                    zlib.createBrotliDecompress(),
-                    fs.createWriteStream(pathNewFile)
-                );
-
-                currentDirectory();
-
+                fs.access(filePathCopy, fs.F_OK, (err) => {
+                    if(err) {
+                        stdout.write(ERROR_MESSAGE_INVALID_INPUT);
+                        currentDirectory();
+                        
+                    } else {
+                        pipeline(
+                            fs.createReadStream(pathFileZip),
+                            zlib.createBrotliDecompress(),
+                            fs.createWriteStream(pathNewFile)
+                        );
+        
+                        currentDirectory();
+                    }
+                })
             } else {
                 stdout.write('File already exists' + '\n');
                 stdout.write(ERROR_MESSAGE_OPERATION);
